dca958476418c251c191953ee5f5403046793e4b
Mike Kenworthy <MKenworthy@covermymeds.com>
2015-01-15 13:22:01 -0500

    My solution ^_^

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,13 +6,22 @@ describe 'SuperFizzBuzz' do
 
   it "returns 'Fizz' when my input is divisible by 3" do
     expect(script.run(3)).to eq "Fizz"
+
+    # expect(script.run(4)).to_not eq "Fizz"
+    # expect(script.run(6)).to eq "Fizz"=end
+
   end
   
   it "returns 'Buzz' when my input is divisible by 5" do
-    #implement your test here
+    expect(script.run(5)).to eq "Buzz"
+    # expect(script.run(10)).to eq "Buzz"
+    # expect(script.run(6)).to_not eq "Buzz"
   end
 
   it "returns 'FizzBuzz' when input is divisible by 3 & 5" do
-    #implement your test here
+    # expect(script.run(3)).to_not eq "FizzBuzz"
+    # expect(script.run(5)).to_not eq "FizzBuzz"
+    # expect(script.run(7)).to_not eq "FizzBuzz"
+    expect(script.run(15)).to eq "FizzBuzz"
   end
 end
