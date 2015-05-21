8bd3d9f6c6e5436c50d5be04807320aac158e0cc
Jonathan Arnett <JArnett@covermymeds.com>
2015-05-21 10:01:22 -0400

    Tests implemented

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,12 +7,12 @@ describe 'SuperFizzBuzz' do
   it "returns 'Fizz' when my input is divisible by 3" do
     expect(script.run(3)).to eq "Fizz"
   end
-  
+
   it "returns 'Buzz' when my input is divisible by 5" do
-    #implement your test here
+    expect(script.run(5)).to eq "Buzz"
   end
 
   it "returns 'FizzBuzz' when input is divisible by 3 & 5" do
-    #implement your test here
+    expect(script.run(15)).to eq "FizzBuzz"
   end
 end
