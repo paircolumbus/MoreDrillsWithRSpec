00bb85dcc23d00ffd4becc048852c491cb6d539c
Peter Mueller <pmueller@covermymeds.com>
2015-05-26 14:02:15 -0400

    break setup into helper method

--- spec/deaf_grandma_spec.rb
+++ spec/deaf_grandma_spec.rb
@@ -4,6 +4,10 @@ require 'deaf_grandma'
 describe 'DeafGrandma' do
   let(:script) { DeafGrandma.new }
 
+  def yell_bye_twice (grandma)
+    2.times { grandma.speak("BYE") }
+  end
+
   it "says 'SPEAK UP SONNY!' when we speak regularly" do
     expect(script.speak("Hi Grandma")).to eq "SPEAK UP SONNY!"
   end
@@ -13,8 +17,7 @@ describe 'DeafGrandma' do
   end
 
   it "says 'SEE YOU LATER SONNY!' after we yell 'BYE' 3 times" do
-    expect(script.speak("BYE")).to eq "NOT SINCE 1964!"
-    expect(script.speak("BYE")).to eq "NOT SINCE 1964!"
+    yell_bye_twice(script)
     expect(script.speak("BYE")).to eq "SEE YOU LATER SONNY!"
   end
 end
