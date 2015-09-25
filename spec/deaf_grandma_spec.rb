7dd52df3487874690b781019ff1a21c895bd8991
G. Wade Johnson <gwadej@covermymeds.com>
2015-09-25 10:13:37 -0400

    Forgot to fix the spec file

--- spec/deaf_grandma_spec.rb
+++ spec/deaf_grandma_spec.rb
@@ -10,9 +10,12 @@ describe 'DeafGrandma' do
   
   it "says 'NOT SINCE 1964!' when we yell" do
     #implement your test here
+    expect(script.speak("HI GRANDMA")).to eq "NOT SINCE 1964!"
   end
 
   it "EXTRA CREDIT: How would you test yelling BYE?" do
-    #implement your test here
+    expect(script.speak("BYE")).to eq "NOT SINCE 1964!"
+    expect(script.speak("BYE")).to eq "NOT SINCE 1964!"
+    # Need some way to Mock exit.
   end
 end
