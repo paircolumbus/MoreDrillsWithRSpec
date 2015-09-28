dcb4c987546b871d44c05d53e4541cdf1d79e76b
Gabriel Escamilla <GEscamilla@covermymeds.com>
2015-09-28 15:03:23 -0400

    Using 7 to test non divisible by 5 or 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -17,6 +17,6 @@ describe 'SuperFizzBuzz' do
   end
   
   it "returns the input number when input isn't divisible by 3, 5, or both" do
-    #implement your test here
+    expect(script.run(7)).to eq 7
   end
 end
