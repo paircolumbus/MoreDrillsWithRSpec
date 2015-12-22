f7967ca73575bcffb86401251dafb5c6cfc9d0a8
Kyle Amyx <masteramyx@gmail.com>
2015-12-22 13:18:25 -0500

    no more explicit returns

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,15 +1,14 @@
 class SuperFizzBuzz
 
   def run(input)
-
    if input % 3 == 0 && input % 5 == 0
-       return 'FizzBuzz'
+        'FizzBuzz'
     elsif input % 3 == 0
-        return 'Fizz'
+        'Fizz'
     elsif input % 5 == 0
-        return 'Buzz'
+        'Buzz'
     else
-        return input
+        input
     end
    end
 end
@@ -17,3 +16,8 @@ end
 #You don't necessarily need to execute this script to complete this challenge, but how would you "run" this method (pun intended) so that it printed a value to the terminal?
 #
 #HINT: it's an instance method.
+
+
+twotime = SuperFizzBuzz.new
+
+puts twotime.run(15)
