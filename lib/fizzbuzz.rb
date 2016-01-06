cdc1698c07b93060ce5efaff08c06b5b6805bdd9
Mary <merussell23@roadrunner.com>
2016-01-05 22:52:06 -0500

    rspecDrill2

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,11 +2,25 @@ class SuperFizzBuzz
 
   def run(input)
 
-    #Implement your code here
-
+    if input % 3 == 0
+      if input % 5 == 0
+        return "FizzBuzz"
+      else
+        return "Fizz"
+      end
+    elsif input % 5 == 0
+      return "Buzz"
+    else
+      return input
+    end
   end
 
 end
+
+
+#to execute as a script:
+temp = SuperFizzBuzz.new
+puts temp.run(5)
 
 #You don't necessarily need to execute this script to complete this challenge, but how would you "run" this method (pun intended) so that it printed a value to the terminal?
 #
