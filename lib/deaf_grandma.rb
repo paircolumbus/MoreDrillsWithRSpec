132349859d923d018e87c0ec4d1e67ddcceab080
Aaron Dawson <adawson927@gmail.com>
2016-01-13 18:05:15 -0500

    cleaned up the code a little

--- lib/deaf_grandma.rb
+++ lib/deaf_grandma.rb
@@ -1,9 +1,12 @@
-#This script is different than FizzBuzz. It should accept user input from the terminal if done correctly. Run it to see what it does then complete the speak method so that it returns & prints the correct thing.
+# This script is different than FizzBuzz. It should accept user input from the
+# terminal if done correctly. Run it to see what it does then complete the
+# speak method so that it returns & prints the correct thing.
 
-#CAREFUL! This script will not exit. Do you know why? You may have to close it with `Ctrl-C` (Mac) if you do not insert an `exit` into your speak method.
+# CAREFUL! This script will not exit. Do you know why? You may have to close
+# it with `Ctrl-C` (Mac) if you do not insert an `exit` into your speak method.
 
 class DeafGrandma
-	attr_reader :bye_counter
+  attr_reader :bye_counter
   def initialize
     @bye_counter = 0
   end
@@ -17,33 +20,22 @@ class DeafGrandma
     end
   end
 
-
   def speak(input)
+    # Implement your code here <<<<<<<<<
+    if input == 'BYE'
+      @bye_counter += 1
+      if @bye_counter >= 3
+        puts 'SEE YOU LATER SONNY!'
+        exit
+      end
+    end
 
-    #Implement your code here <<<<<<<<<
-
-		yell = true
-
-		input.split(//).each do |char|
-			if char != char.capitalize then
-				yell = false
-			end
-		end
-
-		if (input.eql? "BYE")
-			@bye_counter += 1
-			if (@bye_counter >= 3)
-				puts "SEE YOU LATER SONNY!"
-				exit
-			end
-		end
-
-		if (yell)
-			return "NOT SINCE 1964!"
-		else
-			return "SPEAK UP SONNY!"
-  	end
-	end
+    if input == input.upcase
+      'NOT SINCE 1964!'
+    else
+      'SPEAK UP SONNY!'
+    end
+  end
 
   private
 
@@ -52,11 +44,11 @@ class DeafGrandma
   end
 
   def get_user_input
-    print "> "
+    print '> '
     gets.chomp
   end
-
 end
 
-#Uncomment this next line to run your script but BE SURE to comment it, before you try and run your tests.
-#DeafGrandma.new.run!
+# Uncomment this next line to run your script but BE SURE to comment it, before
+#  you try and run your tests.
+# DeafGrandma.new.run!
