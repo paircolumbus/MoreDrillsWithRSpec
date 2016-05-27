e4fa6d65f12bb5aca13465b53522a164c476d412
Kyle Modlich <kmodlich@cmdvmosxlt191.innova.local>
2016-05-27 09:51:37 -0400

    completed

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,15 @@
 class SuperFizzBuzz
 
   def run(input)
-
-    #Implement your code here
-
+	   if input%3 == 0 && input%5 == 0
+	   	 	"FizzBuzz"
+	   	elsif input%3 == 0
+	   		"Fizz"
+	   	elsif input%5 == 0
+	   		"Buzz"
+	   	else
+	   		input
+	   	end
   end
 
 end
