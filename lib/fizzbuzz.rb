23fb25f7928187dc341636155a5260d18bd96b12
Jonathan Stevens <jstevens@covermymeds.com>
2016-08-11 16:31:45 -0400

    Fizz Buzz Specs and Code

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,16 @@
 class SuperFizzBuzz
-
   def run(input)
-
-    #Implement your code here
-
+    case
+      when input % 15 == 0
+        'FizzBuzz'
+      when input % 5 == 0
+        'Buzz'
+      when input % 3 == 0
+        'Fizz'
+      else
+        input
+    end
   end
-
 end
 
 #You don't necessarily need to execute this script to complete this challenge, but how would you "run" this method (pun intended) so that it printed a value to the terminal?
