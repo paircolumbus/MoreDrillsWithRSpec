55b2f5b67bd95f33e7f3413bc329be45f5986317
Drew T. Ritcher <ritchedt@miamioh.edu>
2016-10-24 19:00:18 -0400

    Wrote content for fizzbuzz method

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,10 +2,19 @@ class SuperFizzBuzz
 
   def run(input)
 
-    #Implement your code here
-
+    if input.modulo(3).zero?
+      if input.modulo(5).zero?
+        return "FizzBuzz"
+      else
+        return "Fizz"
+      end
+    elsif input.modulo(5).zero?
+      return "Buzz"
+    else
+      return input
+    end
   end
-
+  
 end
 
 #You don't necessarily need to execute this script to complete this challenge, but how would you "run" this method (pun intended) so that it printed a value to the terminal?
