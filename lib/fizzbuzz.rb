be251826af44f9762890642381890fb33f61cb5b
Drew T. Ritcher <ritchedt@miamioh.edu>
2016-11-05 11:35:12 -0400

    Ready for FizzBuzz refactor analysis
    Would you recommend a different approach to these guard clauses?

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,20 +1,11 @@
 class SuperFizzBuzz
 
   def run(input)
-
-    if input.modulo(3).zero?
-      if input.modulo(5).zero?
-        return "FizzBuzz"
-      else
-        return "Fizz"
-      end
-    elsif input.modulo(5).zero?
-      return "Buzz"
-    else
-      return input
-    end
+    return "FizzBuzz" if input.modulo(3).zero? && input.modulo(5).zero?
+    return "Fizz" if input.modulo(3).zero?
+    return "Buzz" if input.modulo(5).zero?
+    input
   end
-  
 end
 
 #You don't necessarily need to execute this script to complete this challenge, but how would you "run" this method (pun intended) so that it printed a value to the terminal?
