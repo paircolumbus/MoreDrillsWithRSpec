bce5bb6c7249a4b93a1ab6223d649e56d55c19bc
Jackson Taylor <jntaylor92@gmail.com>
2017-01-07 02:09:02 +0000

    Completed DeafGrandma

--- lib/deaf_grandma.rb
+++ lib/deaf_grandma.rb
@@ -19,9 +19,13 @@ class DeafGrandma
 
 
   def speak(input)
-
-    #Implement your code here <<<<<<<<<
-
+    if input.split.count { |i| i.include? "BYE" } == 3
+      "SEE YOU LATER SONNY!"
+    elsif input == input.upcase
+      "NOT SINCE 1964!"
+    else
+      "SPEAK UP SONNY!"
+    end
   end
 
   private
@@ -38,4 +42,4 @@ class DeafGrandma
 end
 
 #Uncomment this next line to run your script but BE SURE to comment it, before you try and run your tests.
-#DeafGrandma.new.run!
+# DeafGrandma.new.run!
