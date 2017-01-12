4a36540c647c615a1be3936458b973f221530177
Leah Musie <lmusie@cmmc02sv1z1g8wl.innova.local>
2017-01-12 15:49:30 -0500

    completed CMM challenge

--- lib/deaf_grandma.rb
+++ lib/deaf_grandma.rb
@@ -5,7 +5,7 @@
 class DeafGrandma
 
   def initialize
-    @bye_counter = 0 
+    @bye_counter = 0
   end
 
   def run!
@@ -19,9 +19,16 @@ class DeafGrandma
 
 
   def speak(input)
-
-    #Implement your code here <<<<<<<<<
-
+    if input != input.upcase
+      "SPEAK UP SONNY!"
+    elsif input == input.upcase
+      if @bye_counter<3
+          @bye_counter=@bye_counter+1
+          "NOT SINCE 1964!"
+      elsif @bye_counter>=3
+          "SEE YOU LATER SONNY!"
+      end
+    end
   end
 
   private
--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,9 +1,15 @@
 class SuperFizzBuzz
 
   def run(input)
-
-    #Implement your code here
-
+    if input%3==0 && input%5==0
+      'FizzBuzz'
+    elsif input%5==0
+      'Buzz'
+    elsif input%3==0
+      'Fizz'
+    else
+      input
+    end
   end
 
 end
