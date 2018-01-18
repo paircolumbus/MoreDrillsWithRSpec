237bfd0a1ab5d666c3981ace9aeb03b0f0b82238
David Hatten <dhatten@cmmc02vt0hzg8wl.innova.local>
2018-01-18 16:12:52 -0500

    buzz test passes

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,16 @@
 class SuperFizzBuzz
 
   def run(input)
+    result = ""
+    case
+      when input % 3 == 0
+        result << "Fizz"
+      when input % 5 == 0
+        result << "Buzz"
+    end
 
-    "Fizz" if input % 3 == 0
-
+    result
   end
-
 end
 
 #You don't necessarily need to execute this script to complete this challenge, but how would you "run" this method (pun intended) so that it printed a value to the terminal?
