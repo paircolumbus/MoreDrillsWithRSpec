4772eaf97e5955d16f7e53a644950838eade0c36
Justin Adkins <jadkins@cmmc02wj11gg8wl.innova.local>
2018-05-29 09:27:04 -0400

    Updated ruby version Completed tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,13 +1,14 @@
 class SuperFizzBuzz
-
   def run(input)
-
-    #Implement your code here
-
+    return 'FizzBuzz' if (input % 3).zero? && (input % 5).zero?
+    return 'Buzz' if (input % 5).zero?
+    return 'Fizz' if (input % 3).zero?
+    input
   end
-
 end
 
-#You don't necessarily need to execute this script to complete this challenge, but how would you "run" this method (pun intended) so that it printed a value to the terminal?
-#
-#HINT: it's an instance method.
+# You don't necessarily need to execute this script to complete this challenge,
+# but how would you "run" this method (pun intended) so that it printed a value
+# to the terminal?
+# HINT: it's an instance method.
+# p SuperFizzBuzz.new.run(1)
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,18 +5,18 @@ describe 'SuperFizzBuzz' do
   let(:script) { SuperFizzBuzz.new }
 
   it "returns 'Fizz' when my input is divisible by 3" do
-    expect(script.run(3)).to eq "Fizz"
+    expect(script.run(3)).to eq 'Fizz'
   end
   
   it "returns 'Buzz' when my input is divisible by 5" do
-    #implement your test here
+    expect(script.run(5)).to eq 'Buzz'
   end
 
   it "returns 'FizzBuzz' when input is divisible by 3 & 5" do
-    #implement your test here
+    expect(script.run(15)).to eq 'FizzBuzz'
   end
   
   it "returns the input number when input isn't divisible by 3, 5, or both" do
-    #implement your test here
+    expect(script.run(17)).to eq 17
   end
 end
