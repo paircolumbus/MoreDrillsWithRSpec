1dc1be563dcd72f5579e9a5adaf97f5ec9d1223c
AlexRichards9595 <arichards@netjets.com>
2018-07-05 09:43:50 -0400

    Finished Fizzbuzz and DeafGrandma. Bundle exec fails the last test while just rspec-ing the file passes all the tests. A little confused there.

--- spec/deaf_grandma_spec.rb
+++ spec/deaf_grandma_spec.rb
@@ -7,12 +7,16 @@ describe 'DeafGrandma' do
   it "says 'SPEAK UP SONNY!' when we speak regularly" do
     expect(script.speak("Hi Grandma")).to eq "SPEAK UP SONNY!"
   end
-  
+
   it "says 'NOT SINCE 1964!' when we yell" do
-    #implement your test here
+    expect(script.speak("HI GRANDMA")).to eq "NOT SINCE 1964!"
   end
 
   it "EXTRA CREDIT: How would you test yelling BYE?" do
-    #implement your test here
+    script.speak("BYE")
+    script.speak("BYE")
+    expect(script.speak("HI GRANDMA")).to eq "NOT SINCE 1964!"
+    script.speak("BYE")
+    expect(script.speak("HI GRANDMA")).to raise_error SystemExit
   end
 end
