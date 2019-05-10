cad5f3f528bdcf1d5698ddcc939c1564f53c6b97
Gary Miller <miller.7856@osu.edu>
2019-05-10 15:19:02 -0400

    done

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,12 +2,19 @@ class SuperFizzBuzz
 
   def run(input)
 
-    #Implement your code here
-
+    if input % 3 == 0 && input % 5 == 0 then
+      "FizzBuzz"
+    elsif input % 3 == 0 then
+      "Fizz"
+    elsif input % 5 == 0 then
+      "Buzz"
+    else
+      input
+    end
   end
-
 end
 
 #You don't necessarily need to execute this script to complete this challenge, but how would you "run" this method (pun intended) so that it printed a value to the terminal?
 #
 #HINT: it's an instance method.
+#p SuperFizzBuzz.new.run(3)
