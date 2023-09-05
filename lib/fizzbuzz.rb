c5e0dbb6b102ae3ae4800164b1e1e9f1e21645a7
Mark Hummel <markhummel@Marks-MacBook-Pro.local>
2023-09-05 19:45:52 -0400

    Fixed syntax error

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,7 +2,7 @@ class SuperFizzBuzz
 
   def run(input)
 
-    f input % 3 == 0 && input % 5 == 0
+    if input % 3 == 0 && input % 5 == 0
       "FizzBuzz"
     elsif input % 5 == 0
       "Buzz"
@@ -11,7 +11,6 @@ class SuperFizzBuzz
     else input
     end
   end
-
 end
 
 #You don't necessarily need to execute this script to complete this challenge, but how would you "run" this method (pun intended) so that it printed a value to the terminal?
